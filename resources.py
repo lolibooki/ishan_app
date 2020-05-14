449a71d7290080143627dc98eee9ac203f6d66bc
Ara Qarib <araqarib@gmail.com>
2020-05-14 04:58:24 +0430

    fix test

--- resources.py
+++ resources.py
@@ -814,7 +814,7 @@ class SubmitQuiz(Resource):
                 else:
                     corrects = 0
                     print(item)
-                    print(user_answers)
+                    print(user_answers[item])
                     for answer in correct_answers[item]["answer"]:
                         print(answer)
                         if answer in user_answers[item]:
